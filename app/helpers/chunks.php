4cd19561c58b803c4001539972c0b4cadb87e833
David Bohn <david@cancrisoft.net>
2012-12-27 15:25:01 +0100

    Updated illuminate/app and removed unneeded code

--- app/helpers/chunks.php
+++ app/helpers/chunks.php
@@ -2,21 +2,17 @@
 
 if(!function_exists('rawChunk')) {
     function rawChunk($name, $type, $scope) {
-        if(func_num_args() > 3) {
-            $properties = func_get_args();
-            unset($properties[0]);
-            unset($properties[1]);
-            unset($properties[2]);
-        } else {
-            $properties = null;
-        }
+        // if(func_num_args() > 3) {
+        //     $properties = func_get_args();
+        //     unset($properties[0]);
+        //     unset($properties[1]);
+        //     unset($properties[2]);
+        // } else {
+        //     $properties = null;
+        // }
         
         if(($class = isValidChunk($type)) !== false) {
             $chunk = new $class();
-
-            /*if(!is_null($properties)) {
-                $chunk->setProperties((array)$properties);
-            }*/
 
             if($chunk->fetchByChunkName($scope, $name)) {
                 return $chunk;
