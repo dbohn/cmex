464679d226f6ab8628abe64115f2428994f84959
David Bohn <david@cancrisoft.net>
2013-02-28 19:53:16 +0100

    Chunks are now autoloaded without composer
    For now the chunks are now autoloaded by the Laravel ClassLoader.
    That's not perfect as it is performing a file_exists on every class it
    loads. So it would be better to create an intelligent ChunkLoader,
    which fetches a list of installed chunks (and can expand it on it's
    own), but therefore we first of all have to finish the chunk structure
    etc. So there is now no "composer dumpautoload" needed anymore for
    adding a new chunk. Yay! :)

---
+++ app/lang/de/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Passwörter müssen mindestens sechs Zeichen lang und bestätigt werden.",
+
+	"user"     => "Es gibt keinen Nutzer mit dieser E-Mail-Adresse.",
+
+	"token"    => "Das Token zur Passwortwiederherstellung ist ungültig.",
+
+);
