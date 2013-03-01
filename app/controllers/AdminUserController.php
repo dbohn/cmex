b352005051fdb2f4c0a1395e778211b13636e51a
Friedrich <friedrich@ueberreiter.com>
2013-03-01 02:07:50 +0100

    Now user can be created and edited

--- app/controllers/AdminUserController.php
+++ app/controllers/AdminUserController.php
@@ -21,7 +21,7 @@ class AdminUserController extends AdminController {
 	 */
 	public function create()
 	{
-		if(self::canCreate()) {
+		if($this->canCreate()) {
 			return View::make('admin.usercreate', array('groups' => Authentication::getGroupProvider()->findAll()));
 		} else {
 			return Redirect::to('admin/user')->with('error', 'Sie haben nicht die nötigen Rechte Benutzer zu erstellen!');
@@ -35,7 +35,52 @@ class AdminUserController extends AdminController {
 	 */
 	public function store()
 	{
-		return Redirect::to('admin/user')->with('error', 'Benutzererstellen noch deaktiviert!');
+		if(!$this->canCreate())
+			return json_encode(array('success' => 0, 'message' => 'Sie haben nicht die nötigen Rechte Benutzer zu erstellen!'));
+		try
+		{
+			$user = Authentication::getUserProvider()->findByCredentials(array(
+				'email' => Input::get('email')
+			));
+			return Redirect::to('admin/user')->with('error', 'Diese E-Mail ist bereits vergeben!');
+		}
+		catch (Cartalyst\Sentry\Users\UserNotFoundException $e)
+		{
+			$error = 0;
+			$message = '';
+			if(Input::has('lastName'))
+				$lastName = Input::get('lastName');
+			if(Input::has('firstName'))
+				$firstName = Input::get('firstName');
+			if(Input::has('email'))
+				$email = Input::get('email');
+			if(Input::has('password'))
+				$password = Input::get('password');
+			
+			if(empty($lastName)) {
+				$error = 1;
+				$message.= 'Bitte geben Sie einen Nachnamen an!<br >';
+			}
+			if(empty($firstName)) {
+				$error = 1;
+				$message.= 'Bitte geben Sie einen Vornamen an!<br >';
+			}
+			if(empty($email)) {
+				$error = 1;
+				$message.= 'Bitte geben Sie eine E-Mail Adresse an!<br >';
+			}
+			if(empty($password)) {
+				$error = 1;
+				$message.= 'Bitte geben Sie ein Passwort an!<br >';
+			}
+			
+			if(!$error) {
+				Authentication::getUserProvider()->create(Input::only('lastName', 'firstName', 'email', 'password'));
+				return Redirect::to('admin/user')->with('success', 'Der Benutzer wurde hinzugefügt!');
+			} else {
+				return Redirect::to('admin/user')->with('error', $message);
+			}
+		}
 	}
 
 	/**
@@ -65,7 +110,7 @@ class AdminUserController extends AdminController {
 		try
 		{
 			$user = Authentication::getUserProvider()->findById($id);
-			if(self::canEdit($id)) {
+			if($this->canEdit($id)) {
 				return View::make('admin.useredit', array(
 					'user' => $user,
 					'groups' => Authentication::getGroupProvider()->findAll())
@@ -90,12 +135,47 @@ class AdminUserController extends AdminController {
 		try
 		{
 			$user = Authentication::getUserProvider()->findById($id);
-			// update algorythm here
-			return Input::json();
+			if($this->canEdit($id)) {
+				$error = 0;
+				$message = '';
+				if(Input::has('lastName'))
+					$lastName = Input::get('lastName');
+				if(Input::has('firstName'))
+					$firstName = Input::get('firstName');
+				if(Input::has('email'))
+					$email = Input::get('email');
+				
+				if(empty($lastName)) {
+					$error = 1;
+					$message.= 'Bitte geben Sie einen Nachnamen an!<br >';
+				}
+				if(empty($firstName)) {
+					$error = 1;
+					$message.= 'Bitte geben Sie einen Vornamen an!<br >';
+				}
+				if(empty($email)) {
+					$error = 1;
+					$message.= 'Bitte geben Sie eine E-Mail Adresse an!<br >';
+				}
+				
+				if(!$error) {
+					$user->last_name = $lastName;
+					$user->first_name = $firstName;
+					$user->email = $email;
+					$user->activated = Input::get('activated', 0);
+					$user->save();
+					
+					return json_encode(array('success' => 1, 'message' => 'Die Änderungen wurden gespeichert!'));
+				} else {
+					return json_encode(array('success' => 0, 'message' => $message));
+				}
+			} else {
+				return json_encode(array('success' => 0, 'message' => 'Sie haben nicht die nötigen Rechte den Benutzer zu bearbeiten!'));
+			}
 		}
 		catch (Cartalyst\Sentry\Users\UserNotFoundException $e)
 		{
-			return null;
+			return json_encode(array('success' => 0, 'message' => 'Der Benutzer wurde nicht gefunden!'));
 		}
 	}
 
