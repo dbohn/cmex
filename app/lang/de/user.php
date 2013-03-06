058de19d9a3bb917c73e4725a7681a433c3b8a84
Friedrich <friedrich@ueberreiter.com>
2013-03-06 18:17:44 +0100

    Language files for user administration

---
+++ app/lang/de/user.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	
+	'created' => 'Der Benutzer wurde erfolgreich erstellt.',
+	
+	'deleted' => 'Der Benutzer wurde erfolgreich gelöscht.',
+	
+	'edited' => 'Die Benutzeränderungen wurden erfolgreich gespeichert.'
+	
+);
---
+++ app/lang/en/user.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	
+	'created' => 'The user was successfully created.',
+	
+	'deleted' => 'The user was successfully deleted.',
+	
+	'edited' => 'The user changes were successfully saved.'
+	
+);
